e59416c26409f1aed8f557348a5ab8224851cff5
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2017-10-30 09:54:12 +0100

    replaced CPM trigger rates fields by instance of new class TCDSTriggerRates, adjusted getters and setters, delegated filling from flashlist to TCDSGlobalInfo.updateFromFlashlist() and clean code to TCDSGlobalInfo.clean()

--- src/main/java/rcms/utilities/daqaggregator/data/TCDSGlobalInfo.java
+++ src/main/java/rcms/utilities/daqaggregator/data/TCDSGlobalInfo.java
@@ -55,22 +55,7 @@ public class TCDSGlobalInfo implements FlashlistUpdatable{
 	private Map<String, Double> deadTimesInstant;
 
 	/*cpm rates*/
-
-	//lumi section to which rates monitoring data correspond
-	private int sectionNumber_rates;
-
-	//totals
-	private double sup_trg_rate_beamactive_total;
-	private double sup_trg_rate_total;
-	private double trg_rate_beamactive_total;
-	private double trg_rate_total;
-
-	//arrays of rate values tt[i], where i=0,...,k (tt0 always at array position 0 etc.)
-	private List<Double> sup_trg_rate_beamactive_tt_values;
-	private List<Double> sup_trg_rate_tt_values;
-	private List<Double> trg_rate_beamactive_tt_values;
-	private List<Double> trg_rate_tt_values;
-
+	private TCDSTriggerRates triggerRates;
 
 	/*pm action counts*/
 	//action[i]_count, , where i=0,...,m (action0 always at array position 0 etc.)
@@ -89,13 +74,11 @@ public class TCDSGlobalInfo implements FlashlistUpdatable{
 		sup_trg_cnt_tt_values = new ArrayList<Integer>();
 		trg_cnt_beamactive_tt_values = new ArrayList<Integer>();
 		trg_cnt_tt_values = new ArrayList<Integer>();
-		sup_trg_rate_beamactive_tt_values = new ArrayList<Double>();
-		sup_trg_rate_tt_values = new ArrayList<Double>();
-		trg_rate_beamactive_tt_values = new ArrayList<Double>();
-		trg_rate_tt_values = new ArrayList<Double>();
 		actionCounts = new ArrayList<Integer>();
 		deadTimes = new HashMap<String, Double>(); //should reset between aggregations in the same DAQAggregator exec.
 		deadTimesInstant = new HashMap<String, Double>(); //should reset between aggregations in the same DAQAggregator exec.
+
+		triggerRates = new TCDSTriggerRates();
 	}
 
 	public Map<String, GlobalTTSState> getGlobalTtsStates() {
@@ -187,67 +170,67 @@ public class TCDSGlobalInfo implements FlashlistUpdatable{
 	}
 
 	public double getSup_trg_rate_beamactive_total() {
-		return sup_trg_rate_beamactive_total;
+		return triggerRates.getSup_trg_rate_beamactive_total();
 	}
 
 	public void setSup_trg_rate_beamactive_total(double sup_trg_rate_beamactive_total) {
-		this.sup_trg_rate_beamactive_total = sup_trg_rate_beamactive_total;
+		triggerRates.setSup_trg_rate_beamactive_total(sup_trg_rate_beamactive_total);
 	}
 
 	public double getSup_trg_rate_total() {
-		return sup_trg_rate_total;
+		return triggerRates.getSup_trg_rate_total();
 	}
 
 	public void setSup_trg_rate_total(double sup_trg_rate_total) {
-		this.sup_trg_rate_total = sup_trg_rate_total;
+		triggerRates.setSup_trg_rate_total(sup_trg_rate_total);
 	}
 
 	public double getTrg_rate_beamactive_total() {
-		return trg_rate_beamactive_total;
+		return triggerRates.getTrg_rate_beamactive_total();
 	}
 
 	public void setTrg_rate_beamactive_total(double trg_rate_beamactive_total) {
-		this.trg_rate_beamactive_total = trg_rate_beamactive_total;
+		triggerRates.setTrg_rate_beamactive_total(trg_rate_beamactive_total);
 	}
 
 	public double getTrg_rate_total() {
-		return trg_rate_total;
+		return triggerRates.getTrg_rate_total();
 	}
 
 	public void setTrg_rate_total(double trg_rate_total) {
-		this.trg_rate_total = trg_rate_total;
+		triggerRates.setTrg_rate_total(trg_rate_total);
 	}
 
 	public List<Double> getSup_trg_rate_beamactive_tt_values() {
-		return sup_trg_rate_beamactive_tt_values;
+		return triggerRates.getSup_trg_rate_beamactive_tt_values();
 	}
 
 	public void setSup_trg_rate_beamactive_tt_values(List<Double> sup_trg_rate_beamactive_tt_values) {
-		this.sup_trg_rate_beamactive_tt_values = sup_trg_rate_beamactive_tt_values;
+		triggerRates.setSup_trg_rate_beamactive_tt_values(sup_trg_rate_beamactive_tt_values);
 	}
 
 	public List<Double> getSup_trg_rate_tt_values() {
-		return sup_trg_rate_tt_values;
+		return triggerRates.getSup_trg_rate_tt_values();
 	}
 
 	public void setSup_trg_rate_tt_values(List<Double> sup_trg_rate_tt_values) {
-		this.sup_trg_rate_tt_values = sup_trg_rate_tt_values;
+		triggerRates.setSup_trg_rate_tt_values(sup_trg_rate_tt_values);
 	}
 
 	public List<Double> getTrg_rate_beamactive_tt_values() {
-		return trg_rate_beamactive_tt_values;
+		return triggerRates.getTrg_rate_beamactive_tt_values();
 	}
 
 	public void setTrg_rate_beamactive_tt_values(List<Double> trg_rate_beamactive_tt_values) {
-		this.trg_rate_beamactive_tt_values = trg_rate_beamactive_tt_values;
+		triggerRates.setTrg_rate_beamactive_tt_values(trg_rate_beamactive_tt_values);
 	}
 
 	public List<Double> getTrg_rate_tt_values() {
-		return trg_rate_tt_values;
+		return triggerRates.getTrg_rate_tt_values();
 	}
 
 	public void setTrg_rate_tt_values(List<Double> trg_rate_tt_values) {
-		this.trg_rate_tt_values = trg_rate_tt_values;
+		triggerRates.setTrg_rate_tt_values(trg_rate_tt_values);
 	}
 
 	public List<Integer> getActionCounts() {
@@ -275,11 +258,11 @@ public class TCDSGlobalInfo implements FlashlistUpdatable{
 	}
 
 	public int getSectionNumber_rates() {
-		return sectionNumber_rates;
+		return triggerRates.getSectionNumber_rates();
 	}
 
 	public void setSectionNumber_rates(int sectionNumber_rates) {
-		this.sectionNumber_rates = sectionNumber_rates;
+		triggerRates.setSectionNumber_rates(sectionNumber_rates);
 	}
 
 	public String getTcdsControllerContext() {
@@ -415,110 +398,7 @@ public class TCDSGlobalInfo implements FlashlistUpdatable{
 		}
 
 		if (flashlistType == FlashlistType.TCDS_CPM_RATES){
-			//reset arrays to flush old entries
-			sup_trg_rate_beamactive_tt_values = new ArrayList<Double>();
-			sup_trg_rate_tt_values = new ArrayList<Double>();
-			trg_rate_beamactive_tt_values = new ArrayList<Double>();
-			trg_rate_tt_values = new ArrayList<Double>();
-
-			//set ls number
-			sectionNumber_rates = flashlistRow.get("section_number").asInt();
-
-			//set totals
-			sup_trg_rate_beamactive_total = flashlistRow.get("sup_trg_rate_beamactive_total").asDouble();
-			sup_trg_rate_total = flashlistRow.get("sup_trg_rate_total").asDouble();
-			trg_rate_beamactive_total = flashlistRow.get("trg_rate_beamactive_total").asDouble();
-			trg_rate_total = flashlistRow.get("trg_rate_total").asDouble();
-
-
-			//pattern for column name
-			String pattern;
-
-			//tt[0] to tt[max] are not parsed in guaranteed order, so this map is needed to later store in correct order
-			Map<Integer,Double> positionToValue;
-
-
-			//fill sup_trg_rate_beamactive_tt_values
-			pattern = "sup_trg_rate_beamactive_tt\\d+";
-
-			positionToValue = new HashMap<Integer, Double>();
-
-			for (Iterator<String> fieldNameIter = flashlistRow.fieldNames(); fieldNameIter.hasNext() ;){
-				String fieldName = fieldNameIter.next();
-				if (!Pattern.matches(pattern, fieldName)){
-					continue;
-				}
-
-				int i = Integer.parseInt(fieldName.substring(26));
-				positionToValue.put(i, flashlistRow.get(fieldName).asDouble());
-			}
-
-			//adding values from map to array, in tt[0]->tt[max] ordering
-			for (int i=0;i<positionToValue.size();i++){
-				sup_trg_rate_beamactive_tt_values.add(positionToValue.get(i));
-			}
-
-
-			//fill sup_trg_rate_tt_values
-			pattern = "sup_trg_rate_tt\\d+";
-
-			positionToValue = new HashMap<Integer, Double>();
-
-			for (Iterator<String> fieldNameIter = flashlistRow.fieldNames(); fieldNameIter.hasNext() ;){
-				String fieldName = fieldNameIter.next();
-				if (!Pattern.matches(pattern, fieldName)){
-					continue;
-				}
-
-				int i = Integer.parseInt(fieldName.substring(15));
-				positionToValue.put(i, flashlistRow.get(fieldName).asDouble());
-			}
-
-			//adding values from map to array, in tt[0]->tt[max] ordering
-			for (int i=0;i<positionToValue.size();i++){
-				sup_trg_rate_tt_values.add(positionToValue.get(i));
-			}
-
-			//fill trg_rate_beamactive_tt_values
-			pattern = "trg_rate_beamactive_tt\\d+";
-
-			positionToValue = new HashMap<Integer, Double>();
-
-			for (Iterator<String> fieldNameIter = flashlistRow.fieldNames(); fieldNameIter.hasNext() ;){
-				String fieldName = fieldNameIter.next();
-				if (!Pattern.matches(pattern, fieldName)){
-					continue;
-				}
-
-				int i = Integer.parseInt(fieldName.substring(22));
-				positionToValue.put(i, flashlistRow.get(fieldName).asDouble());
-			}
-
-			//adding values from map to array, in tt[0]->tt[max] ordering
-			for (int i=0;i<positionToValue.size();i++){
-				trg_rate_beamactive_tt_values.add(positionToValue.get(i));
-			}
-
-
-			//fill trg_rate_tt_values
-			pattern = "trg_rate_tt\\d+";
-
-			positionToValue = new HashMap<Integer, Double>();
-
-			for (Iterator<String> fieldNameIter = flashlistRow.fieldNames(); fieldNameIter.hasNext() ;){
-				String fieldName = fieldNameIter.next();
-				if (!Pattern.matches(pattern, fieldName)){
-					continue;
-				}
-
-				int i = Integer.parseInt(fieldName.substring(11));
-				positionToValue.put(i, flashlistRow.get(fieldName).asDouble());
-			}
-
-			//adding values from map to array, in tt[0]->tt[max] ordering
-			for (int i=0;i<positionToValue.size();i++){
-				trg_rate_tt_values.add(positionToValue.get(i));
-			}
+		  this.triggerRates.updateFromFlashlist(flashlistType, flashlistRow);
 		}
 
 		if (flashlistType == FlashlistType.TCDS_CPM_DEADTIMES){
@@ -609,15 +489,6 @@ public class TCDSGlobalInfo implements FlashlistUpdatable{
 		sup_trg_cnt_total = 0;
 		trg_cnt_beamactive_total = 0;
 		trg_cnt_total = 0;
-		sup_trg_rate_beamactive_tt_values = new ArrayList<Double>();
-		sup_trg_rate_tt_values = new ArrayList<Double>();
-		trg_rate_beamactive_tt_values = new ArrayList<Double>();
-		trg_rate_tt_values = new ArrayList<Double>();
-		sectionNumber_rates = 0;
-		sup_trg_rate_beamactive_total = 0;
-		sup_trg_rate_total = 0;
-		trg_rate_beamactive_total = 0;
-		trg_rate_total = 0;
 		fillNumber = 0;
 		sectionNumber_deadtimes = 0;
 
@@ -625,6 +496,9 @@ public class TCDSGlobalInfo implements FlashlistUpdatable{
 		deadTimesInstant = new HashMap<String, Double>();
 		actionCounts = new ArrayList<Integer>();
 
+		triggerRates = new TCDSTriggerRates();
+
+		triggerRates.clean();
 
 	}
 
@@ -636,4 +510,11 @@ public class TCDSGlobalInfo implements FlashlistUpdatable{
 		this.deadTimesInstant = deadTimesInstant;
 	}
 
+	public TCDSTriggerRates getTriggerRates() {
+		return triggerRates;
+	}
+
+	public void setTriggerRates(TCDSTriggerRates triggerRates) {
+		this.triggerRates = triggerRates;
+	}
 }

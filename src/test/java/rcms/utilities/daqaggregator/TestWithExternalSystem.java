72b051fa7c60e10adee86fbdc458ac5eb7a3bcc3
Andre Holzner <Andre.Georg.Holzner@cern.ch>
2017-11-13 18:23:52 +0100

    added class to tag junit tests for tests relying on external systems

---
+++ src/test/java/rcms/utilities/daqaggregator/TestWithExternalSystem.java
@@ -0,0 +1,9 @@
+package rcms.utilities.daqaggregator;
+
+/**
+ * Marker for JUnit tests which need the presence of an external system
+ * and should therefore not be run during release building.
+ */
+public interface TestWithExternalSystem {
+	
+}
